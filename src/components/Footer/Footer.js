a2452fa6eaa5012d69403df8d72528c23c16b1f7
Patrice-H <https://github.com/>
2022-05-07 22:05:16 +0200

    Add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import logoFooter from '../../assets/general/logo-footer.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <img src={ logoFooter } alt='logo' />
+            <p>
+                <FontAwesomeIcon icon={ faCopyright } />
+                &nbsp;
+                2020 Kasa. All rights reserved
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
