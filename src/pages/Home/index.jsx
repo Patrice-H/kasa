df93834c9c0dcf5c9ff3315eb9569a8def5a616f
Patrice-H <https://github.com/Patrice-H>
2022-05-15 18:03:04 +0200

    Update home page with json file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,22 @@
+import React,{useState,useEffect} from 'react';
 import Header from '../../components/Header';
 import Banner from "../../components/Banner";
 import Thumb from '../../components/Thumb';
 import Footer from '../../components/Footer';
-import { accomodations } from '../../datas/accomodations';
 import './Home.css';
 
-/*
-let titles = [];
-fetch('../datas/accomodations.json')
-.then(response => response.json())
-.then(datas => datas.forEach(data => titles.push(data.title)))
-.catch(err => console.log('Error : ', err))
-
-fetch('../datas/accomodations.json')
-    .then(async response => {
-        //data = await response.json();
-    })
-*/
 const Home = () => {
     const activePage = 'home';
+    const [data,setData]=useState([]);
+    const getData=()=>{
+        fetch('./datas/accomodations.json')
+        .then(response => response.json())
+        .then(allData => setData(allData))
+        .catch(err => console.log('Error : ', err))
+    };
+    useEffect(()=>{
+        getData()
+    },[]);
 
     return (
         <>
@@ -26,7 +24,7 @@ const Home = () => {
             <Banner activePage={activePage} />
             <section className='gallery'>
                 <ul>
-                    {accomodations.map((accomodation) => (
+                    {data && data.length>0 && data.map((accomodation) => (
                         <li key={ accomodation.id }>
                             <Thumb 
                                 title={ accomodation.title } 
