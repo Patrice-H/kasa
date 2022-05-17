1b476c12c2a2937ee37cc7483ceceb4c850ee0e8
Patrice-H <https://github.com/Patrice-H>
2022-05-17 16:33:00 +0200

    Adding accomodation page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import About from './pages/About';
 import Home from './pages/Home';
+import Accomodation from './pages/Accomodation';
 import Error404 from './pages/Error404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,6 +14,7 @@ root.render(
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/accomodation/:accomodationId' element={<Accomodation />}></Route>
         <Route path='*' element={<Error404 />}></Route>
       </Routes>
     </Router>
---
+++ src/pages/Accomodation/index.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React,{useState,useEffect} from 'react';
+import {useNavigate} from "react-router-dom"
+import Header from "../../components/Header";
+import TagsBar from '../../components/TagsBar';
+import DropDown from '../../components/DropDown';
+import Profile from '../../components/Profile';
+import RatingBar from '../../components/RatingBar';
+import Footer from '../../components/Footer';
+import './Accomodation.css';
+import Carousel from '../../components/Carousel';
+
+const Accomodation = () => {
+    
+    const pathname = window.location.pathname.split('/');
+    const accomodationId = pathname[pathname.length - 1];
+    const [data,setData]=useState([]);
+    const navigate = useNavigate();
+
+    const getData=()=>{
+        fetch('../datas/accomodations.json')
+        .then(response => response.json())
+        .then(allData => {
+            const result = allData.find(Data => Data.id === accomodationId)
+            typeof result === 'undefined' ? navigate('/error404') : setData([result])
+        })
+        .catch(err => console.log('Error : ', err))
+    };
+    useEffect(()=>{
+        getData();
+    }, []);
+
+    return (
+        <>
+            <Header />
+            {data && data.length>0 && data.map((accomodation) => (
+                <section key={accomodation.id} className='accomodation'>
+                    <Carousel pictures={accomodation.pictures}/>
+                    <h1>{accomodation.title}</h1>
+                    <p className='location'>{accomodation.location}</p>
+                    <TagsBar tags={accomodation.tags}/>
+                    <div className='content'>
+                        <DropDown name='Description' content={[accomodation.description]} />
+                        <DropDown name='Équipements' content={accomodation.equipments} />
+                    </div>
+                    <div className='informations'>
+                        <Profile host={accomodation.host} />
+                        <RatingBar rating={accomodation.rating} />
+                    </div>
+                </section>  
+            ))}
+            <Footer />
+        </>
+    );
+}
+
+export default Accomodation;
