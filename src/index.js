31f7b85dd527791c77420d3ca0bd7a7416f170b4
Patrice-H <https://github.com/Patrice-H>
2022-05-22 23:19:28 +0200

    Arranging code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
 import About from './pages/About';
 import Home from './pages/Home';
 import Accomodation from './pages/Accomodation';
 import Error404 from './pages/Error404';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
